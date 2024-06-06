47be9b0e304fea973f2919b6b413abe348ead078
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2024-06-06 15:40:36 -0600

    Adding ApiUrls for APIs

---
+++ src/main/java/com/fitnesstracker/utils/ApiUrls.java
@@ -0,0 +1,7 @@
+package com.fitnesstracker.utils;
+
+public class ApiUrls {
+    public static final String API_URL_WORKOUTS= "/workouts";
+    public static final String API_URL_GET_WORKOUT_BY_ID = "/{id}";
+    public static final String API_URL_UPDATE_WORKOUT = "/{id}";
+}
